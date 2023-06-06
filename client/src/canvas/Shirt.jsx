5e1c4553d06169ccb7e2640171e74f9eaaef93d9
cotnals99 <su.chae@icloud.com>
2023-06-06 15:02:37 +0700

    Basic threeJS setting

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSnapshot } from 'valtio'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { Decal, useGLTF, useTexture } from '@react-three/drei'
+
+import state from '../store'
+
+const Shirt = () => {
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF('/shirt_baked.glb')
+
+  const logoTexture = useTexture(snap.logoDecal)
+  const fullTexture = useTexture(snap.fullDecal)
+
+  return (
+    <group>
+      <mesh
+        cashShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+
+      </mesh>
+    </group>
+  )
+}
+
+export default Shirt
