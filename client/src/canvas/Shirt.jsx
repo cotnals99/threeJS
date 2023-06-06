3d7988beaeef90def3ae9a03fdbfaa8dff9f3870
cotnals99 <su.chae@icloud.com>
2023-06-06 16:12:00 +0700

    FIX: typo in labert1

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -13,9 +13,10 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal)
   const fullTexture = useTexture(snap.fullDecal)
 
-  useFrame((state, delta) => easing.dampC(materials.lamber1.color, snap.color, 0.25, delta))
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta))
 
   const stateString = JSON.stringify(snap)
+
   return (
     <group
       key={stateString}
