1741d6cf96c939df354b65e95426f77274ab26d9
cotnals99 <su.chae@icloud.com>
2023-06-07 10:01:54 +0700

    Backdrop, Shirt, CameraRig

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -22,7 +22,7 @@ const Shirt = () => {
       key={stateString}
     >
       <mesh
-        cashShadow
+        castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
