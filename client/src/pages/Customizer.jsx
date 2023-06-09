78e0dc5468f1ff280a7cc3a616297bf0dfcf5ba7
cotnals99 <su.chae@icloud.com>
2023-06-09 09:57:40 +0700

    AIPicker OpenAI setting completion

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,6 +55,21 @@ const Customizer = () => {
 
     try {
       // call our backend to generate an ai image
+      setGeneratingImg(true)
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json()
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
 
     } catch (error) {
       alert(error)
@@ -85,6 +100,7 @@ const Customizer = () => {
       default:
         state.isLogoTexture = true
         state.isFullTexture = false
+        break;
     }
 
     // after setting the state, activeFilterTab is updated
